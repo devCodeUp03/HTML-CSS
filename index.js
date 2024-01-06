e2eff45375cc38dcf89d8512f7975db8aee06656
Devashish Upreti <devashup68@gmail.com>
2024-01-06 10:00:20 +0545

    java-script started

---
+++ index.js
@@ -0,0 +1,34 @@
+console.log("I am a leader");
+
+greeting = "Hello";
+greeting = "Namaste"; /* variable overriding just like in css */
+console.log(greeting, "Bruce Lee");
+
+var title = "mern" /* advice: try to not use var */
+let duration = "3 months"
+const price = "Rs 16000"
+
+
+/* data types
+    primitive data types
+    string, integer, boolean
+    undefined // data type is not sure alot of it is found in javaScript
+    null // empty 
+
+
+    non-primitive data types/ collections/ reference types
+        -array(note: in core array is also an object in js)
+        >usually collection of similar data types but it works in js
+        note: it isn't the right way
+        -object
+
+        let <variable> = [<element1>, <element2>, <element3>] 
+*/
+
+let course1 = "mern";
+let course2 = "machine Learning";
+
+let courses = [course1, course2, "graphics design", "qa"];
+console.log(courses);
+
+
---
+++ variable.js
@@ -0,0 +1,43 @@
+// alert("js connected")
+/* console.log("js connected")
+console.log("I become whatever I think all day long.") */
+
+"thought"
+
+i = "I love" /* variable: container which stores value  */
+/*
+ naming conventions
+ snake_case // in python very common
+ camelCase // in js very common
+ kebab-case
+*/
+/* 
+console.log(i, "being alone")
+console.log(i, "being comfortable with uncomfortable situation")
+console.log(i, "putting myself in trouble")
+console.log(i, "reading books")
+
+three_level_of_changes = "outcome system identity"
+
+console.log(three_level_of_changes) */
+
+courseTitle = "MERN Stack"
+studentNumber = 20
+time = "3pm - 4pm"
+console.log("Course Title", courseTitle)
+console.log("Number of Students", studentNumber)
+console.log("Timing", time)
+
+/*
+ Data types(common data types )
+    text -> string
+    number -> integer(whole number) / float / double(decimal numbers)
+    true/false -> boolean
+*/
+
+luckyNumber = 1
+console.log("My lucky number is: ",luckyNumber);
+/* let var const */
+
+
+
